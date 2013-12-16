ba3106839a5151f916e5c10d1fbbc2208b4014a0
yun <yun@yun-E425>
2013-12-15 20:47:11 -0600

    do post

--- src/com/yun/homeplusplus/GetResidentsApi.java
+++ src/com/yun/homeplusplus/GetResidentsApi.java
@@ -14,7 +14,7 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 public class GetResidentsApi extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 
 		StringBuilder sb = new StringBuilder();
